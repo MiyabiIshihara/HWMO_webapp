6ccf56bd5eeb95cc3171f24f8355a6783b16e503
Niklas Lollo <nicklollo2@gmail.com>
2018-03-02 16:10:12 -0800

    shiny app functional with ggplot and sf

--- wireframe/server.R
+++ wireframe/server.R
@@ -6,17 +6,20 @@ library(RColorBrewer)
 library(DT)
 library(shiny)
 library(sf)
-library(spdep)
+#library(spdep)
 
 # load data
-haz_dat = st_read("../data/WHA Metadata/WHA2015.shp")
-census_dat = st_read("../data/Census_Tract_All_Data/Census_Tract_All_Data.shp")
-fire_dat = st_read("../data/Fire History/HI_Wildfires1.shp")
-comm_dat <- read_csv("../data/comm_input.csv") # change file path beore uploading to Shiny
+haz_data = st_read("../data/WHA Metadata/WHA2015.shp") %>%
+  select(SUBD_TOT, VEG_TOT, BLDG_TOT, FIREHAZTOT,FIREPROTOT,
+         # Acres, AREA, PERIMETER, CAR_Rating, 
+         AreaName, Island, geometry) %>%
+  gather(key = haz_category, value = amount, -c(AreaName, Island, geometry))
 
-# find min/max for slider
-#dat %>% group_by(dimension) %>% filter(!is.na(value)) %>% dplyr::summarise(min = min(value), max = max(value)) %>% write_csv("slider.csv")
-slider <- read_csv("slider.csv")
+#haz_dat = read_csv("../data/haz_dat.csv")
+#census_dat = st_read("data/Census_Tract_All_Data/Census_Tract_All_Data.shp")
+#fire_dat = st_read("data/Fire History/HI_Wildfires1.shp")
+#comm_dat = read_csv("data/comm_input.csv")
+
 # light grey boundaries
 l <- list(color = toRGB("grey"), width = 0.5)
 
@@ -24,116 +27,83 @@ l <- list(color = toRGB("grey"), width = 0.5)
 g <- list(
  showframe = FALSE,
  showcoastlines = FALSE,
- projection = list(type = 'Mercator')
+ projection = list(type = '26904')
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output, session) {
-  
+function(input, output, session) {
   # landing page
-  observeEvent(once = TRUE,ignoreNULL = FALSE, ignoreInit = FALSE, eventExpr = dat, { 
-    # event will be called when histdata changes, which only happens once, when it is initially calculated
-    showModal(modalDialog(
-      #title = "Ocean Health Index Map and Data Explorer", 
-      h1('Welcome!'),
-      tags$p('This webapp displays information for the Hawaii Wildfire Management Organzation (HWMO)'),
-      useShinyjs(),  # Set up shinyjs
-      actionButton("go_button", "Read more..."),
-      hidden(
-        p(id = "element", tags$br(), tags$b("The HWMO does this"), "By this.", tags$br(), tags$br(), "And this", tags$em("(Sources)"), "and here", tags$em("(more sources)."), tags$br(), tags$br(), "And more." )
-      )  )) } 
-  )
-  
-  # load hidden text
+  observeEvent(once = TRUE,
+               ignoreNULL = FALSE, 
+               ignoreInit = FALSE, 
+               eventExpr = haz_data, { 
+                 # event will be called when histdata changes, which only happens once, when it is initially calculated
+                 showModal(modalDialog(
+                   h1('Welcome!'),
+                   tags$p('This webapp displays information for the Hawaii Wildfire Management Organzation (HWMO)'),
+                   useShinyjs(),  # Set up shinyjs
+                   actionButton("go_button", "Read more..."),
+                   hidden(
+                     p(id = "element", 
+                       tags$br(), 
+                       tags$b("The HWMO does this"), 
+                       "By this.", 
+                       tags$br(), 
+                       tags$br(), 
+                       "And this", 
+                       tags$em("(Sources)"), 
+                       "and here", 
+                       tags$em("(more sources)."), 
+                       tags$br(), 
+                       tags$br(), 
+                       "And more.")))) }
+               )
+  # load hidden text in welcome dialogue box
   observeEvent(input$go_button, {
     show("element")
   })
   
-  
-  # Reactive values for slider input 
-  my_reactives <- reactiveValues( react_ind = list(0, 100) )
-  
-  # this first event is a hack that allows 
-  observe( {
-    input$go_button
-    my_reactives$react_ind <- input$v_4
-  })
-  observe( {
-    input$v_1
-    my_reactives$react_ind <- input$v_1
-  })
-  observe( {
-    input$v_2
-    my_reactives$react_ind <- input$v_2
-  })
-  observe( {
-    input$v_3
-    my_reactives$react_ind <- input$v_3
-  })
-  observe( {
-    input$v_4
-    my_reactives$react_ind <- input$v_4
-  })
-  observe( {
-    input$v_5
-    my_reactives$react_ind <- input$v_5
-  })
-  observe( {
-    input$v_6
-    my_reactives$react_ind <- input$v_6
-  })
-  
-  
+  ## this first event is a hack that allows 
+  #observe( {
+  #  input$go_button
+  #  my_reactives$react_ind <- input$v_4
+  #})
+
   temp <- reactive( {     # filter dat by user input
-    dat %>% filter(goal == input$goal & 
-                     dimension == input$dimension & 
-                     value >= my_reactives$react_ind[1] & value <= my_reactives$react_ind[2] ) })
-  
-  temp_mod <- reactive( {     # filter dat by user input
-    dat_mod %>% filter(goal == input$goal & 
-                         dimension == input$dimension) })
-  
+    haz_data %>% filter(haz_category == input$haz_category) 
+    })
+
   # Main Map
-  output$map <- renderPlotly({
-    
-    plot_geo( temp() ) %>% 
-      add_trace(
-        z = ~value, 
-        zmin = slider$min[slider$dimension == input$dimension],
-        zmax = slider$max[slider$dimension == input$dimension],
-        color = ~value, colors = 'Blues',
-        frame = ~scenario,
-        text = ~region_name, locations = ~a3_code, marker = list(line = l)
-      ) %>%
-      colorbar(title = 'Value', tickprefix = '') %>%
-      layout(
-        title = paste('Ocean Health Index:<br>',input$goal, input$dimension),
-        geo = g) %>% 
-      
-      animation_button(
-        x = 1, xanchor = "right", y = 0, yanchor = "bottom")
+  output$map <- renderPlot({
+    temp() %>%
+    ggplot() +
+      geom_sf() +
+      aes(fill = amount) + 
+      theme(panel.grid.major = element_line(color = "white")) +
+      scale_fill_gradientn(colors = sf.colors(20))
     
   })
-  
-  # Main Map
-  output$map_mod <- renderPlotly({
-    
-    plot_geo( temp_mod() ) %>% 
-      add_trace(
-        z = ~estimate, 
-        zmin = -3,
-        zmax = 3,
-        color = ~estimate_cat, 
-        colorscale = list(c(-1, 'rgb(0,0,255)'), c(0, 'rgb(0,0,0)'), c(1, 'rgb(255,0,0)')),
-        reversescale = TRUE,
-        text = ~region_name, locations = ~a3_code, marker = list(line = l)
-      ) %>%
-      colorbar(title = 'Regression Coefficient', tickprefix = '') %>%
-      layout(
-        title = "Change from 2012 - 2017",
-        geo = g)
-    
-  })
+  #output$map <- renderPlotly({
+  #  
+  #  plot_geo( temp()
+  #            ) %>% 
+  #    add_trace(
+  #      x = st_coordinates(temp())[,1],
+  #      y = st_coordinates(temp())[,2],
+  #      z = ~amount, 
+  #      zmin = 30,
+  #      zmax = 0,
+  #      color = ~amount, 
+  #      colors = 'Blues',
+  #      text = ~AreaName, 
+  #      locations = ~st_geometry(temp()),
+  #      marker = list(line = l)
+  #    ) %>%
+  #    colorbar(title = 'Score', 
+  #             tickprefix = '') %>%
+  #    layout(title = "Hazards in Hawaii",
+  #           geo = g)
+  #})
   
   # Plot
   output$dt <- DT::renderDataTable({ temp() })
@@ -143,7 +113,7 @@ server <- function(input, output, session) {
     # This function returns a string which tells the client browser what name to use when saving the file.
     filename = function() {
       paste0(
-        paste(input$goal, input$dimension, input$react_ind[1], input$react_ind[2], sep = "_"),
+        paste(input$haz_category),
         ".csv")
     },
     
@@ -158,7 +128,7 @@ server <- function(input, output, session) {
   output$download_all_data <- downloadHandler(
     # This function returns a string which tells the client browser what name to use when saving the file.
     filename = function() {
-      paste0("all_ohi_dat", ".csv")
+      paste0("haz_dat", ".csv")
     },
     
     # This function should write data to a file given to it by the argument 'file'.
--- wireframe/ui.R
+++ wireframe/ui.R
@@ -6,54 +6,25 @@ library(RColorBrewer)
 library(DT)
 library(shiny)
 
-ui <- 
-  dashboardPage(skin = "black", 
-                
-                dashboardHeader(title = "HWMO", titleWidth = 300),
-                
-                # sidebar panels for 'goal' and 'dimension'
-                dashboardSidebar(width = 300,
-                                 
-                                 radioButtons("goal", 
-                                              label = "Goal", 
-                                              choices = c('Artisanal opportunities' = 'AO',
-                                                          'Biodiversity' = 'BD',
-                                                          'Coastal Protection' = 'CP',
-                                                          'Carbon Storage' = 'CS',
-                                                          'Clean Water' = 'CW'
-                                                          ),
-                                              selected = c("CW")),
-                                 radioButtons("dimension", 
-                                              label = "Dimension",
-                                              choices = c('future' = 'future','pressures' = 'pressures'),
-                                              selected = "score"),
-                                 
-                                 # sliders for 'value' are conditional on the selected 'dimension'
-                                 
-                                 conditionalPanel(
-                                   condition = "input.dimension == 'future'",
-                                   sliderInput("v_1", label = "Value",
-                                               min = slider$min[slider$dimension == "future"],
-                                               max = slider$max[slider$dimension == "future"],
-                                               value = c(slider$min[slider$dimension == "future"], 
-                                                         slider$max[slider$dimension == "future"]),
-                                               step = 1)),
-                                 
-                                 conditionalPanel(
-                                   condition = "input.dimension == 'pressures'",
-                                   sliderInput("v_2", label = "Value",
-                                               min = slider$min[slider$dimension == "pressures"],
-                                               max = slider$max[slider$dimension == "pressures"],
-                                               value = c(slider$min[slider$dimension == "pressures"], 
-                                                         slider$max[slider$dimension == "pressures"]),
-                                               step = 1))
-                ),
-                
-                
-                
-                # Body
-                dashboardBody(
-                  tags$head( tags$style(HTML("
+
+  header <- dashboardHeader(
+    title = "HWMO", titleWidth = 300
+  )
+  
+  sidebar <- dashboardSidebar(width = 300,
+                              radioButtons("haz_category",
+                                           label = "Hazard Category",
+                                           choices = c('Fire Hazard' = 'FIREHAZTOT',
+                                                       'Fire Protection' = 'FIREPROTOT',
+                                                       'Building Hazard' = 'BLDG_TOT',
+                                                       'Vegetation Hazard' = 'VEG_TOT',
+                                                       'Subdivision Hazard' = 'SUBD_TOT'
+                                                        ),
+                                           selected = c("FIREHAZTOT")
+                                           )
+                              )
+
+body <- dashboardBody(tags$head(tags$style(HTML("
                                              blockquote {
                                              padding: 10px 20px;
                                              margin: 0 0 20px;
@@ -61,28 +32,44 @@ ui <-
                                              border-left: 5px solid #eee;
                                              }
                                              "))),
-                  fluidRow(
-                    tabBox(width = 12, height = NULL,
-                           tabPanel("Map", value = 1,
-                                    tags$style(type = "text/css", "#map {height: calc(100vh - 100px) !important;}"),
-                                    plotlyOutput("map")),
-                           tabPanel("5 year change", value = 2,
-                                    tags$style(type = "text/css", "#map {height: calc(100vh - 100px) !important;}"),
-                                    plotlyOutput("map_mod")),
-                           tabPanel("Data", 
-                                    fluidRow(
-                                      column(width = 12,
-                                             box(title = NULL, width = NULL,
-                                                 DT::dataTableOutput("dt")),
-                                             box(title = "Download Data", width = NULL,
-                                                 downloadButton("download_data", "Download Selected Data"),
-                                                 tags$br(),
-                                                 tags$br(),
-                                                 downloadButton("download_all_data", "Download All Data")))
-                                    )),
-                           tabPanel(title = "About", 
-                                    includeMarkdown("../about.md"))
-                    )
-                  )
-                  )
-                  )
+                      fluidRow(
+                        tabBox(width = 12,
+                               height = NULL,
+                               tabPanel("Map",
+                                        value = 1,
+                                        tags$style(
+                                        type = "text/css", 
+                                        "#map {height: calc(100vh - 100px) !important;}"),
+                                        plotOutput("map")
+                                        ),
+                               tabPanel("Data",
+                                        fluidRow(
+                                          column(width = 12,
+                                                 box(title = NULL,
+                                                     width = NULL,
+                                                     DT::dataTableOutput("dt")),
+                                                 box(title = "Download Data",
+                                                     width = NULL,
+                                                     downloadButton("download_data",
+                                                                    "Download Selected Data"),
+                                                     tags$br(),
+                                                     tags$br(), 
+                                                     downloadButton("download_all_data",
+                                                                    "Download All Data")
+                                                     )
+                                                 )
+                                          )
+                                        ),
+                               tabPanel(title = "About",
+                                        includeMarkdown("../about.md")
+                                        )
+                               )
+                        )
+                      )
+                    
+dashboardPage(
+  skin = "black",
+  header,
+  sidebar,
+  body
+)
